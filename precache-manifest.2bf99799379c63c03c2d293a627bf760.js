fb6ebbc87dea7cf53599efe092e542b9332faeea
Meet Gojiya <meetgojiya@Meets-MacBook-Pro.local>
2025-04-02 11:57:28 +0530

    Updates

---
+++ precache-manifest.2bf99799379c63c03c2d293a627bf760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facf2272bd6c2cd3414ce4d45f0066b2",
+    "url": "/E-Commerce-Website/index.html"
+  },
+  {
+    "revision": "bc45e56d1650662b2dbe",
+    "url": "/E-Commerce-Website/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "a9cf18726ccd1eaaa0dc",
+    "url": "/E-Commerce-Website/static/css/main.4289a7c4.chunk.css"
+  },
+  {
+    "revision": "bc45e56d1650662b2dbe",
+    "url": "/E-Commerce-Website/static/js/2.43fba54e.chunk.js"
+  },
+  {
+    "revision": "a9cf18726ccd1eaaa0dc",
+    "url": "/E-Commerce-Website/static/js/main.db02d842.chunk.js"
+  },
+  {
+    "revision": "60b85966c28e6b6b0d56",
+    "url": "/E-Commerce-Website/static/js/runtime~main.5b4da22e.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/E-Commerce-Website/static/media/logo.9f7e506d.svg"
+  }
+]);
